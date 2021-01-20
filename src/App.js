1cce2a9ef83f6ce6d57afb0dd5e66aaeb9b77ff5
RonFrontWeb <62059074+RonFrontWeb@users.noreply.github.com>
2021-01-20 13:50:26 +0100

    PrimaryMenuBar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import './App.css';
 import LoadingPage from './LoadingPage';
 import { Router } from "@reach/router";
+import PrimaryMenubar from './PrimaryMenubar';
+import SecondaryMenubar from './SecondaryMenubar';
+import "./variables.css"
 
 function App() {
   return (
     // <Router>
-   
+    
     // </Router>
-  <LoadingPage/>
+    <>
+    <LoadingPage/>
+  <PrimaryMenubar/>
+  <SecondaryMenubar/>
+  </>
   );
 }
 
 export default App;
+
